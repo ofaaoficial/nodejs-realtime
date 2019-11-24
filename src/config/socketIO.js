6d633d7e85f9b4500775a44dff1207eab1b53593
ofaaoficial <oscarfamado@gmail.com>
2019-11-23 22:23:24 -0500

    finished real time draw with socket io

--- src/config/socketIO.js
+++ src/config/socketIO.js
@@ -1,5 +1,16 @@
 module.exports = io => {
-    io.on('connection', () => {
-        console.log('New user connected');
-    })
+
+    const history = [];
+
+    io.on('connection', socket => {
+
+        for(let i in history)
+            socket.emit('drawing', history[i]);
+
+        socket.on('drawing', data => {
+            history.push(data);
+            io.emit('drawing', data);
+        })
+    });
+
 };
--- src/public/assets/js/app.js
+++ src/public/assets/js/app.js
@@ -12,34 +12,44 @@ const on = (event, callback, el = document) => el.addEventListener(event, callba
     const wHeight = canvas.height = window.innerHeight;
 
     const mouse = {
-        position: {
-            x: 0,
-            y: 0
+        line: {
+            moveTo: { x: 0, y: 0 },
+            lineTo: { x: 0, y: 0 }
         },
         actions: {
             click: false,
             move: false
         },
-        handdlers(){
+        handlers(){
             on('mousedown', () => this.actions.click = true, canvas);
             on('mouseup', () => this.actions.click = false, canvas);
              on('mousemove', (e) => {
-                 this.position.x = e.clientX / wWidth;
-                 this.position.y = e.clientY / wHeight;
+                 [this.line.lineTo.x, this.line.lineTo.y] = [e.offsetX, e.offsetY];
                  this.actions.move = true;
              }, canvas);
             this.loop();
         },
         loop(){
             if(this.actions.click && this.actions.move){
-                cl('exec');
-
+                socket.emit('drawing', this.line);
+                this.actions.move = false;
             }
+            [this.line.moveTo.x, this.line.moveTo.y] = [this.line.lineTo.x, this.line.lineTo.y];
+
             requestAnimationFrame(this.loop.bind(this));
+        },
+        listen(data){
+            ctx.beginPath();
+                ctx.lineWidth = 2;
+                ctx.moveTo(data.moveTo.x, data.moveTo.y);
+                ctx.lineTo(data.lineTo.x, data.lineTo.y);
+            ctx.stroke();
         }
+
     };
 
-    mouse.handdlers();
+    mouse.handlers();
+    socket.on('drawing', mouse.listen);
 
     cl('Script is works');
 })();
