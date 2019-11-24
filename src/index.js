792067c7b882c012a2975dc01487540bfcf9008a
ofaaoficial <oscarfamado@gmail.com>
2019-11-23 20:14:17 -0500

    basic configuration (port, public folder static)

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static( path.join(__dirname, 'public') ));
+
+app.listen(app.get('port'), () => {
+    console.log(`Server run at http://localhost:${app.get('port')}`);
+});
